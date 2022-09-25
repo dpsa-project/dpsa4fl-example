11aebc600657ea81349184ef58f28eae6eb1cd11
Maxim Urschumzew <u.maxim@live.de>
2022-09-25 11:31:53 +0200

    Initial commit.

---
+++ dpsa4fl_example/main.py
@@ -0,0 +1,6 @@
+
+import dpsa4fl
+
+def call_sum(a,b):
+    return dpsa4fl.sum_as_string(a,b)
+
---
+++ tests/test_dpsa4fl_example.py
@@ -0,0 +1,5 @@
+from dpsa4fl_example import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
